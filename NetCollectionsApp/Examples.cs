8362041308dca9e180ebc2d219051201e19cd9c9
maxefimov <efimov.top.academy@gmail.com>
2024-01-18 19:12:32 +0300

    18-01-24

--- NetCollectionsApp/Examples.cs
+++ NetCollectionsApp/Examples.cs
@@ -9,6 +9,116 @@ namespace NetCollectionsApp
 {
     static class Examples
     {
+        static public void IndexRangeExample()
+        {
+            List<int> intList = new() { 5, 11, 9, 21, 65, 2 };
+
+            Console.WriteLine(intList[intList.Count - 2]);
+            Index index = 2;
+            Index index2 = ^2;
+            Console.WriteLine(intList[index]);
+            Console.WriteLine(intList[^1]);
+
+            Range range1 = new Range(2, 5);
+            Range range2 = 2..4;
+            int start = 2;
+            int end = 4;
+            Range range3 = ..end;
+            Range range4 = index2..;
+
+            var subList = intList[range4];
+            foreach (var item in subList)
+                Console.Write($"{item} ");
+            Console.WriteLine();
+        }
+
+        static public void DictionaryExample()
+        {
+            Dictionary<string, int> regions = new Dictionary<string, int>()
+            {
+                /*{ "Tula", 71 },
+                { "Moscow", 90 },*/
+                ["Tula"] = 71,
+                ["Moscow"] = 90
+            };
+
+            KeyValuePair<string, int> kaluga = new KeyValuePair<string, int>("Kaluga", 40);
+            regions.Add(kaluga.Key, kaluga.Value);
+
+            foreach (var region in regions)
+                Console.WriteLine($"Region: {region.Key}, Code: {region.Value}");
+
+            if (regions.TryGetValue("Moscow", out int code))
+                Console.WriteLine(code);
+
+            regions.TryAdd("Moscow", 95);
+
+            foreach (var region in regions)
+                Console.WriteLine($"Region: {region.Key}, Code: {region.Value}");
+
+            regions["Moscow"] = 95;
+
+            foreach (var region in regions)
+                Console.WriteLine($"Region: {region.Key}, Code: {region.Value}");
+
+            regions["Voroneg"] = 36;
+
+            foreach (var region in regions)
+                Console.WriteLine($"Region: {region.Key}, Code: {region.Value}");
+        }
+
+        static public void ListExample()
+        {
+            Random random = new Random();
+
+            List<int> listInt = new List<int>();
+            List<int> listInt2 = new(10);
+            List<int> listInt3 = [];
+
+            for (int i = 0; i < 10; i++)
+                listInt.Add(random.Next() % 100);
+
+            foreach (int i in listInt)
+                Console.Write($"{i} ");
+            Console.WriteLine();
+
+            for (int i = 0; i < listInt.Count; i++)
+                Console.Write($"{listInt[i]} ");
+
+            /*
+             * void Add(T item) - add to back list
+             * void AddRange(IEnumerable<T> collection) - add other collection
+
+             * int BinarySearch(T item) - return index finded item or number < 0
+
+             * void CopyTo(T[] array) - copy list to Array
+             * bool Contains(T item) - true if item contains in list
+             * void Clear() - deleted all items from list
+
+             * T? Find(Predicate<T> match) - return reference to item
+             * T? FindLast(Predicate<T> match) - return reference to item
+             * List<T> FindAll(Predicate<T> match) - return list
+
+             * int IndexOf(T item) - return index of item or number < 0
+             * int LastIndexOf(T item) - return index of item or number < 0
+
+             * List<T> GetRange(int index, int count) - return range length of count, from index
+
+             * void Insert(int index, T item) - indert item to list at index position
+             * void InsertRange(int index, IEnumerable<T> collection) - indert collection to list at index position
+
+             * bool Remove(T item) - delete item from list (true if item contains)
+             * void RemoveAt(int index) - delete item at index position
+             * void RemoveRange(int index, int count) - delete rande from index and count items
+             * 
+             * void Reverse()
+             * void Reverse(int index, int count)
+             * void Sort()
+             * void Sort(IComparer<T> comparer)
+             * 
+             */
+
+        }
 
         static public void ArrayExample()
         {
--- NetCollectionsApp/Program.cs
+++ NetCollectionsApp/Program.cs
@@ -1,57 +1,33 @@
 ﻿using NetCollectionsApp;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
-Random random = new Random();
+//ObservableCollection<string> oc = new();
 
-List<int> listInt = new List<int>();
-List<int> listInt2 = new(10);
-List<int> listInt3 = [];
+Stack<int> stack = new Stack<int>();
+Queue<string> queue = new Queue<string>();
 
-for (int i = 0; i < 10; i++)
-    listInt.Add(random.Next() % 100);
+stack.Push(100);
+stack.Push(200);
+stack.Push(300);
 
-foreach(int i in listInt)
-    Console.Write($"{i} ");
+foreach (var item in stack)
+    Console.WriteLine(item);
+
+while(stack.Count > 0)
+    Console.Write($"{stack.Pop()} ");
 Console.WriteLine();
 
-for(int i = 0; i < listInt.Count; i++)
-    Console.Write($"{listInt[i]} ");
 
-/*
- * void Add(T item) - add to back list
- * void AddRange(IEnumerable<T> collection) - add other collection
- 
- * int BinarySearch(T item) - return index finded item or number < 0
- 
- * void CopyTo(T[] array) - copy list to Array
- * bool Contains(T item) - true if item contains in list
- * void Clear() - deleted all items from list
- 
- * T? Find(Predicate<T> match) - return reference to item
- * T? FindLast(Predicate<T> match) - return reference to item
- * List<T> FindAll(Predicate<T> match) - return list
- 
- * int IndexOf(T item) - return index of item or number < 0
- * int LastIndexOf(T item) - return index of item or number < 0
- 
- * List<T> GetRange(int index, int count) - return range length of count, from index
- 
- * void Insert(int index, T item) - indert item to list at index position
- * void InsertRange(int index, IEnumerable<T> collection) - indert collection to list at index position
- 
- * bool Remove(T item) - delete item from list (true if item contains)
- * void RemoveAt(int index) - delete item at index position
- * void RemoveRange(int index, int count) - delete rande from index and count items
- * 
- * void Reverse()
- * void Reverse(int index, int count)
- * void Sort()
- * void Sort(IComparer<T> comparer)
- * 
- */
+queue.Enqueue("AAAAA");
+queue.Enqueue("BBBBB");
+queue.Enqueue("CCCCC");
 
+foreach (var item in queue)
+    Console.WriteLine(item);
 
-
-
+while (queue.Count > 0)
+    Console.Write($"{queue.Dequeue()} ");
+Console.WriteLine();
 
 
